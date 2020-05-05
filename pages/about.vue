b75f306bc267041c2f6e6be2bdaec1fce3edaaf7
Jake Potrebic <jake.m.potrebic@gmail.com>
2020-05-05 12:17:32 -0700

    added about page

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <blockquote class="blockquote">
+        Nomad's Notebook is a helper web application for tracking quality locations in Last Oasis.
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
